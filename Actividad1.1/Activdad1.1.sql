9605cb9a7f192b5b30245917966279a2a89697c3
Joaquin <jachaval@users.noreply.github.com>
2021-03-29 01:00:36 -0300

    Edicion

--- Actividad1.1/Activdad1.1.sql
+++ Actividad1.1/Activdad1.1.sql
@@ -3,26 +3,26 @@ GO
 use actividad3
 GO
 create table Carreras(
-	ID varchar(4) not null primary key,
+	ID char(4) not null primary key,
 	Nombre varchar(50) not null,
-	FechaCreacion date not null check(FechaCreacion <= getdate()),
+	'Fecha Creación' date not null check("Fecha Creación" <= getdate()),
 	Mail varchar(40) not null,
 	Nivel varchar(12) not null check(Nivel = 'Diplomatura' or Nivel = 'Pregrado' or Nivel = 'Grado' or Nivel = 'Posgrado'),
 )
 GO
 create table Materias(
 	ID bigint not null primary key identity(1,1),
-	IDCarrera varchar(4) not null foreign key references Carreras(ID),
+	IDCarrera char(4) not null foreign key references Carreras(ID),
 	Nombre varchar(30) not null,
-	CargaHoraria int not null check(CargaHoraria>0),
+	"Carga Horaria" int not null check("Carga Horaria">0),
 )
 GO
 create table Alumnos(
 	Legajo bigint not null primary key identity(1000,1),
-	IDCarrera varchar(4) not null foreign key references Carreras(ID),
+	IDCarrera char(4) not null foreign key references Carreras(ID),
 	Apellidos varchar(30) not null,
 	Nombres varchar(30) not null,
-	FechaNacimiento date not null check(FechaNacimiento <= getdate()),
+	"Fecha de Nacimiento" date not null check("Fecha de Nacimiento" <= getdate()),
 	Mail varchar(40) not null unique,
 	Telefono int null,
 )
